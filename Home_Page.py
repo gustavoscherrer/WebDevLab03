e304240974fd4daabcc32cc6850aad3046a289f5
gustavoscherrer <gusscherrer13@gmail.com>
2025-04-02 12:39:19 -0400

    WebDevPages
    AllPages

---
+++ Home_Page.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab03")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Team XX, Web Development - Section X")
+st.subheader("Team Member 1, Team Member 2")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. **Home Page**: Provides an overview of our Hunger Games-themed app and what each page offers.
+2. **Victors**: Explore a searchable database of past Hunger Games victors. Filter by district, weapon of choice, and survival status.
+3. **Arena Battler**: Simulate battles between two tributes using their kill counts, injuries, and weapons.
+4. **CapitolBot**: Chat with an AI bot trained on Hunger Games data to answer questions about Panem’s deadliest.
+
+Happy Hunger Games and may the odd be ever in your favor!
+""")
+
